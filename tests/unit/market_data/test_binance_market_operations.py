4634bbbd7d61f9de4ebae04eb3a46c3a00e796e9
vikramaditya <vikramaditya91@gmail.com>
2020-08-25 19:13:00 +0200

    Added some test cases

--- tests/unit/market_data/test_binance_market_operations.py
+++ tests/unit/market_data/test_binance_market_operations.py
@@ -16,6 +16,8 @@ def binance_market_operator():
         ("1d", client.Client.KLINE_INTERVAL_1DAY, None),
         ("1h", client.Client.KLINE_INTERVAL_1HOUR, None),
         ("3d", client.Client.KLINE_INTERVAL_3DAY, None),
+        ("1M", client.Client.KLINE_INTERVAL_1MONTH, None),
+        ("1m", client.Client.KLINE_INTERVAL_1MINUTE, None),
         ("31d", None, AssertionError)
     ])
 def test_match_binance_enum(binance_market_operator, string_of_time, binance_enum, exception):
