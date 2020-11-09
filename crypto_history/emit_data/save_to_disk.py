3857a4b4cb0839faf7dc0e5d50173b50d2a70cb7
vikramaditya <vikramaditya91@gmail.com>
2020-11-09 18:43:07 +0100

    Tried to split the large funcs into smaller

--- crypto_history/emit_data/save_to_disk.py
+++ crypto_history/emit_data/save_to_disk.py
@@ -3,7 +3,7 @@ import pathlib
 from sqlalchemy.orm import sessionmaker
 import xarray as xr
 import pandas as pd
-from typing import Union
+from typing import Union, List
 from abc import ABC, abstractmethod
 from sqlalchemy import create_engine
 from crypto_history.utilities.general_utilities import register_factory
@@ -70,11 +70,10 @@ class ConcreteJSONWriter(ConcreteAbstractDiskWriter):
 
 
 class ConcreteSQLiteWriter(ConcreteAbstractDiskWriter):
-    """Writes the fields to a SQLite database. It writes a table with
-    the name: COIN_HISTORY_{ohlcv}_{reference_asset}_{candle}.
+    """Writes the fields to a SQLite database.
 
     Warnings: It will raise an error if there are multiple types of candles
-        """
+    """
     def __init__(self,
                  sqlite_db_path,
                  ):
@@ -85,14 +84,65 @@ class ConcreteSQLiteWriter(ConcreteAbstractDiskWriter):
         )
 
     @staticmethod
-    def yield_db_name_from_dataset(dataarray,
-                                   ohlcv_to_deposit):
+    def get_df_from_da(dataarray: xr.DataArray,
+                       reference_asset: str,
+                       ohlcv_field: str) -> pd.DataFrame:
+        """
+        Gets a df from the dataarray whose axis are \
+        col-> coins, rows-> timestamps
+        Args:
+            dataarray: xr.DataArray which will be converted
+            reference_asset: reference asset from the dataarray \
+             Note that it can have only 1 reference asset
+            ohlcv_field: field for flattening
+
+        Returns:
+            pd.DataFrame which contains the 2D
+            information of the dataarray
+
+        """
+        to_convert_to_pd = dataarray.loc[
+                           :, reference_asset, :, ohlcv_field
+                           ]
+        return to_convert_to_pd.transpose().to_pandas()
+
+    @staticmethod
+    def get_sql_table_name(dataarray: xr.DataArray,
+                           reference_asset: str,
+                           ohlcv_field: str
+                           ):
+        """
+        Gets the name of the SQL table. Name is in the format:
+        COIN_HISTORY_{ohlcv}_{reference_asset}_{candle}.
+        Args:
+            dataarray: xr.DataArray which should be converted
+            reference_asset: str, reference asset
+            ohlcv_field: str, ohlcv_field
+
+        Returns:
+            str, a string with the table of the SQL table
+
+        """
+        non_nan_values = dataarray.loc[
+                         :, reference_asset, :, "weight"
+                         ].to_pandas().dropna()
+        unique_values = pd.unique(
+            non_nan_values.values.flatten()
+        ).tolist()
+        assert len(unique_values) == 1, \
+            f"More than 1 type of weights found. {unique_values}"
+        return f"COIN_HISTORY_{ohlcv_field}_" \
+               f"{reference_asset}_{unique_values[0]}"
+
+    def yield_db_name_from_dataset(self,
+                                   dataarray: xr.DataArray,
+                                   ohlcv_fields: List[str]):
         """
         Yields the df, name of the table from the dataarray
         Args:
             dataarray: xr.DataArray which has to be entered \
              in the SQLite DB
-            ohlcv_to_deposit: list of fields to deposit
+            ohlcv_fields: list of fields to deposit
 
         Yields: df, name where each df corresponds to one \
             field from the ohlcv fields
@@ -100,24 +150,19 @@ class ConcreteSQLiteWriter(ConcreteAbstractDiskWriter):
         """
         for reference_asset_da in dataarray.reference_assets:
             reference_asset = reference_asset_da.values.tolist()
-            for ohlcv in ohlcv_to_deposit:
-                to_convert_to_pd = dataarray.loc[:, reference_asset, :, ohlcv]
-                df = to_convert_to_pd.transpose().to_pandas()
-
-                non_nan_values = dataarray.loc[
-                                 :, reference_asset, :, "weight"
-                                 ].to_pandas().dropna()
-                unique_values = pd.unique(
-                    non_nan_values.values.flatten()
-                ).tolist()
-                assert len(unique_values) == 1, \
-                    f"More than 1 type of weights found. {unique_values}"
-                name = f"COIN_HISTORY_{ohlcv}_{reference_asset}_{unique_values[0]}"
-                yield df, name
+            for ohlcv_field in ohlcv_fields:
+                df = self.get_df_from_da(dataarray,
+                                         reference_asset,
+                                         ohlcv_field)
+                table_name = self.get_sql_table_name(dataarray,
+                                                     reference_asset,
+                                                     ohlcv_field
+                                                     )
+                yield df, table_name
 
     def write_coin_history_dataset_to_disk(self,
-                                           dataarray,
-                                           ohlcv_to_deposit):
+                                           dataarray: xr.DataArray,
+                                           ohlcv_to_deposit: List[str]):
         """
         Writes the coin history to SQLite DB
 
